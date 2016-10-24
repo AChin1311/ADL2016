b380d0f2534c51ddbf6dceaa5f3038067c59831d
AChin1311 <b02902034@ntu.edu.tw>
2016-10-24 20:45:30 +0800

    skip_gram

---
+++ hw1/skipgram.py
@@ -0,0 +1,26 @@
+from tensorflow.models.embedding import word2vec_optimized as word2vec
+
+import numpy as np
+import tensorflow as tf
+
+def main(_):
+  """Train a word2vec model."""
+  opts = word2vec.Options()
+  with tf.Graph().as_default(), tf.Session() as session:
+    with tf.device("/cpu:0"):
+      model = word2vec.Word2Vec(opts, session)
+    for _ in xrange(opts.epochs_to_train):
+      model.train()  # Process one epoch
+    
+    emb = session.run(model._w_in)  
+    print(model._id2word)
+    with open("output_skipopt", "r+") as f:
+      for i in range(len(model._id2word)):
+        s = model._id2word[i] + " " + " ".join("{:f}".format(x) for x in emb[i]) + "\n"
+        f.write(s)
+        print(s)
+
+if __name__ == "__main__":
+  gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+  sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+  tf.app.run()
