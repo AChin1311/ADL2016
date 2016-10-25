af1f8dccbf7c1d4527fec0c83aa9dc7da5048311
AChin1311 <b02902034@ntu.edu.tw>
2016-10-25 15:49:46 +0800

    phase2

--- hw1/phase2.py
+++ hw1/phase2.py
@@ -9,16 +9,20 @@ def main(_):
   with tf.Graph().as_default(), tf.Session() as session:
     with tf.device("/cpu:0"):
       model = word2vec.Word2Vec(opts, session)
+      model.read_analogies()
     for _ in xrange(opts.epochs_to_train):
       model.train()  # Process one epoch
+      model.eval()
 
     emb = session.run(model._w_in)  
-    with open("output_skipopt", "r+") as f:
+    print("write file")
+    with open("output_ptt", "r+") as f:
       for i in range(len(model._id2word)):
         s = model._id2word[i] + " " + " ".join("{:f}".format(x) for x in emb[i]) + "\n"
         f.write(s)
+    print("finish")
 
 if __name__ == "__main__":
-  gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+  gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.2)
   sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
   tf.app.run()
