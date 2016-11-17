069201c84681f8f9a60587b672ef93f87055ad55
AChin1311 <b02902034@ntu.edu.tw>
2016-11-17 17:30:20 +0800

    upload model/report

--- hw2/run_rnn_e.sh
+++ hw2/run_rnn_e.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# please download the model at https://drive.google.com/file/d/0B6z4WeJetpHgeTBaTGF1cmkzaDg/view?usp=sharing
-# unzip the file and name the folder "weights"
-# put "weights" folder in the same directory with rvnn.py and preprocess.py
-
-mkdir trees
-
 # preprocess testing data
 python3 preprocess.py $2
 
